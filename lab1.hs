42d349588c26be5a7075917f135d59db4e71e385
Mark Hakansson <mark.hakansson@live.se>
2020-04-09 09:00:42 +0200

    (Lab 1) add functions to generate subsets

--- lab1.hs
+++ lab1.hs
@@ -1,1 +1,18 @@
-main = putStrLn "Hello world!"
+-- sorting
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = quicksort [y | y <- xs, y <= x] ++ [x] ++ quicksort [y | y <- xs, y > x]
+
+-- create subsets starting from the last element in the list
+subsetsLastElement :: [Integer] -> [[Integer]] 
+subsetsLastElement [] = error "Input is empty!"
+subsetsLastElement [x] = [[x]]
+subsetsLastElement xs = [xs] ++ subsetsLastElement (tail xs) 
+
+-- generate all possible subsets of a list 
+generateSubsets :: [Integer] -> [[Integer]]
+generateSubsets [] = error "Input is empty!"
+generateSubsets [x] = [[x]]
+generateSubsets xs = subsetsLastElement xs ++ generateSubsets (init xs)
+
+
