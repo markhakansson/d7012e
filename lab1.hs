4ac63be19bcf29fbac8b2dc9606810df5eaf54cd
Mark Hakansson <mark.hakansson@live.se>
2020-04-14 15:12:20 +0200

    Fix conflicts with lab1

--- lab1.hs
+++ lab1.hs
@@ -1,7 +1,13 @@
--- sorting
-quicksort :: (Ord a) => [a] -> [a]
+-- D7012E Lab 1 
+-- Get the smallest k sets of a list
+
+-- sorts the lists using the first element as pivot
+quicksort :: Ord a => [a] -> [a]
 quicksort [] = []
-quicksort (x:xs) = quicksort [y | y <- xs, y <= x] ++ [x] ++ quicksort [y | y <- xs, y > x]
+quicksort (x:xs) = quicksort x1 ++ [x] ++ quicksort x2
+    where
+        x1 = [i | i <- xs, i <= x]
+        x2 = [i | i <- xs, i > x]
 
 -- create subsets starting from the last element in the list
 subsetsLastElement :: [Integer] -> [[Integer]] 
@@ -9,8 +15,12 @@ subsetsLastElement [] = error "Input is empty!"
 subsetsLastElement [x] = [[x]]
 subsetsLastElement xs = [xs] ++ subsetsLastElement (tail xs) 
 
--- generate all possible subsets of a list 
-generateSubsets :: [Integer] -> [[Integer]]
+-- create tuple with the last index and subsets ending at that index
+subsetsWithLastIndex :: [Integer] -> [(Int, [Integer])]
+subsetsWithLastIndex xs = map (\x -> (length xs - 1, x)) (subsetsFromLastElement xs)
+
+-- generate all possible subsets of a list in a tuple together with the last index 
+generateSubsets :: [Integer] -> [(Int,[Integer])]
 generateSubsets [] = error "Input is empty!"
 generateSubsets [x] = [[x]]
 generateSubsets xs = subsetsLastElement xs ++ generateSubsets (init xs)
@@ -32,7 +42,8 @@ sortSubsetsBySize :: [Integer] -> [(Integer, [Integer])]
 sortSubsetsBySize = quicksort . computeSizes . generateSubsets
 
 -- gets the k number of smallest subsets of a list
-smallestKSets :: Int -> [Integer] -> [(Integer, [Integer])]
+smallestKSets :: Int -> [Integer] -> [(Integer, Int, [Integer])]
+smallestKSets _ [] = error "Input is empty!"
 smallestKSets k xs = take k (sortSubsetsBySize xs)
 
 -- formatting functions
@@ -46,8 +57,8 @@ formatTuples [] = ""
 formatTuples xs = formatTuple (head xs) ++ formatTuples (tail xs)
 
 -- main program
-main :: Int -> [Integer] -> IO ()
-main k xs = do
+program :: Int -> [Integer] -> IO ()
+program k xs = do
     printHeader
     let res = smallestKSets k xs
     let res_format = formatTuples res
