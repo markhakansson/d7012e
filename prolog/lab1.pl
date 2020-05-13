1cd8f80a170542c7a595a44cf4fcd0a2a4f39a8c
Mark Hakansson <mark.hakansson@live.se>
2020-05-13 08:04:41 +0200

    (labP1) polising on lab1

--- prolog/lab1.pl
+++ prolog/lab1.pl
@@ -1,5 +1,5 @@
 % Package delivery lab
-% Mark Hakansson
+% by Mark Hakansson
 
 % Some information:
 % 
@@ -14,69 +14,72 @@
 % --Move--
 % Works like this:
 % State is the next state if Condition is true. To reach the next State, a Move is done.
+% Take actions also check that the inventory is not full (i.e. more than 2 items)
 
 % Move to room 2 (from room1)
-move(Condition, State, Action) :-
+action(Condition, State, Action) :-
     Condition = state(r1, inventory, Brass, Package),
     State = state(r2, inventory, Brass, Package),
     Action = "move to r2".
 
 % Move to room 1 (from room2)
-move(Condition, State, Action) :-
+action(Condition, State, Action) :-
     Condition = state(r2, inventory, Brass, Package),  
     State = state(r1, inventory, Brass, Package),
     Action = "move to r1".
 
 % Move to room 1 (from room3)
-move(Condition, State, Action) :-
+action(Condition, State, Action) :-
     Condition = state(r3, Steel, inventory, Package),
     State = state(r1, Steel, inventory, Package),
     Action = "move to r1".
 
 % Move to room 3 (from room 1)
-move(Condition, State, Action) :-
+action(Condition, State, Action) :-
     Condition = state(r1, Steel, inventory, Package),
     State = state(r3, Steel, inventory, Package),
     Action = "move to r3".
 
 % Take steel key
-move(Condition, State, Action) :-
+action(Condition, State, Action) :-
     Condition = state(Room, Room, Brass, Package),
     State = state(Room, inventory, Brass, Package),
     State \= state(_, inventory, inventory, inventory),
     Action = "take steel key".
 % Take brass key 
-move(Condition, State, Action) :-
+action(Condition, State, Action) :-
     Condition = state(Room, Steel, Room, Package),
     State = state(Room, Steel, inventory, Package),
     State \= state(_, inventory, inventory, inventory),
     Action = "take brass key".
 % Take package
-move(Condition, State, Action) :-
+action(Condition, State, Action) :-
     Condition = state(Room, Steel, Brass, Room),
     State = state(Room, Steel, Brass, inventory),
     State \= state(_, inventory, inventory, inventory),
     Action = "take package".
 
 % Drop steel key
-move(Condition, State, Action) :-
+action(Condition, State, Action) :-
     Condition = state(Room, inventory, Brass, Package),
     State = state(Room, Room, Brass, Package),
     Action = "drop steel key".
 % Drop brass key
-move(Condition, State, Action) :-
+action(Condition, State, Action) :-
     Condition = state(Room, Steel, inventory, Package),
     State = state(Room, Steel, Room, Package),
     Action = "drop brass key".
 % Drop package
-move(Condition, State, Action) :-
+action(Condition, State, Action) :-
     Condition = state(Room, Steel, Brass, inventory),
     State = state(Room, Steel, Brass, Room),
     Action = "drop package".
 
+% Solves the problem for a state, with a max recursion step of N.
+% Default state = state(r1,r1,r2,r3)
 solveR(state(_, _, _, r2), _, ["finished"]). 
 solveR(State, N, [Action|Trace]) :-
     N > 0,
-    move(State, NextState, Action),
+    action(State, NextState, Action),
     solveR(NextState, N - 1, Trace).
 
